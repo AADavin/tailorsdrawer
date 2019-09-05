6e1803833232919461cf7608bc6245c7c988eee9
Adrian A. Davin <aaredav@gmail.com>
2019-09-05 12:25:25 +1000

    correcting script

--- prepare_protein_database.py
+++ prepare_protein_database.py
@@ -8,7 +8,7 @@ _, path = sys.argv
 fasta_files = [x for x in os.listdir(path) if x.endswith(".faa")]
 
 sp = 0
-seq = 0
+seqn = 0
 
 
 with open("./AllProteins.fasta", "w") as f1:
@@ -20,12 +20,12 @@ with open("./AllProteins.fasta", "w") as f1:
                 line2 = "\t".join([msp, acc]) + "\n"
                 f2.write(line2)
                 for h, seq in fasta_parser(os.path.join(path, fasta_file)):
-                    mseq = str(seq)
+                    mseq = str(seqn)
                     line1 = ">" + msp + "_" + mseq + "\n"
                     f1.write(line1)
                     f1.write(seq + "\n")
                     f3.write(msp + "_" + mseq + "\t" + h + "\n")
-                    seq +=1
+                    seqn +=1
             sp += 1
 
 
