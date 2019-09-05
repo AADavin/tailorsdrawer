b4d6dc80e1c0dae9b33c3ae420a883c99e194c67
Adrian A. Davin <aaredav@gmail.com>
2019-09-05 16:43:13 +1000

    correcting script

--- careful_copy.py
+++ careful_copy.py
@@ -7,7 +7,7 @@ def careful_copy(inpath, outpath, extension):
     mfiles2 = {x for x in os.listdir(outpath) if x.endswith(extension)}
 
     for mfile in (mfiles1 - mfiles2):
-        print("cp %s %s" % ((os.path.join(outpath, mfile), os.path.join(outpath, mfile))))
+        print("cp %s %s" % ((os.path.join(inpath, mfile), os.path.join(outpath, mfile))))
 
 
 args = sys.argv
